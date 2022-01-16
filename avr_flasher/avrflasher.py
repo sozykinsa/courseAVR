bb826fc11045bc512690fcf03f2e21a2c983d999
serge <sozykinsa@susu.ru>
2022-01-16 18:45:17 +0500

    Work in Windows

--- avr_flasher/avrflasher.py
+++ avr_flasher/avrflasher.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
+import subprocess
 import sys
+import time
+
+from pathlib import Path
 
 from qtpy.QtCore import QCoreApplication, Qt
 from qtpy.QtWidgets import QApplication, QMessageBox, QMainWindow, QFileDialog
@@ -13,10 +17,16 @@ class mainWindow(QMainWindow):
         super(mainWindow, self).__init__(*args)
         self.ui = Ui_form()
         self.ui.setupUi(self)
+        self.hex_fname = ""
 
     def setup_ui(self):
         # buttons
         self.ui.programmButton.clicked.connect(self.programm)
+        self.ui.selectAvrdude.clicked.connect(self.select_avr_dude)
+        self.ui.selectAVRdude.clicked.connect(self.select_hex)
+        self.ui.saveSettings.clicked.connect(self.save_settings)
+        self.ui.loadDefSetWin.clicked.connect(self.load_def_settings_win)
+        self.ui.loadDefSetLin.clicked.connect(self.load_def_settings_lin)
 
     def show_error(self, e):
         msg = QMessageBox()
@@ -27,8 +37,43 @@ class mainWindow(QMainWindow):
         msg.exec_()
 
     def programm(self):
-        # create model
-        df = 5
+        exe = self.ui.avrdudeEXE.toPlainText()
+        param = self.ui.avrdudeParams.toPlainText()
+        hex_name = self.ui.hexFile.text()
+        conf = self.ui.avrdudeConf.toPlainText()
+        task = str(exe) + ' -C"' + str(conf) + '" ' + str(param) + ' -U flash:w:"' + str(self.hex_fname) + ':i"'  # #, " -C" + str(conf)
+        print(task)
+        process = subprocess.Popen(task, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=None, text=True)
+        while process.poll() is None:
+            output_line = str(process.stderr.readline())  # .stdout.readline())
+            self.ui.textLog.appendPlainText(str(output_line).rstrip())
+
+    def select_avr_dude(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file')[0]
+        if os.path.exists(fname):
+            self.ui.avrdudeEXE.setText(fname)
+
+    def select_hex(self):
+        self.hex_fname = QFileDialog.getOpenFileName(self, 'Open file')[0]
+        if os.path.exists(self.hex_fname):
+            self.ui.hexFile.setText(self.hex_fname)
+
+    def load_def_settings_win(self):
+        fname = r"C:\Program Files (x86)\Arduino\hardware\tools\avr\bin\avrdude.exe"
+        self.ui.avrdudeEXE.setText(fname)
+        conf = r"C:\Program Files (x86)\Arduino\hardware\tools\avr\etc\avrdude.conf"
+        self.ui.avrdudeConf.setText(conf)
+        param = "-v -patmega328p -carduino -PCOM9 -b57600 -D"
+        self.ui.avrdudeParams.setText(param)
+
+    def load_def_settings_lin(self):
+        fname = "text3"
+        self.ui.avrdudeEXE.setText(fname)
+        param = "text4"
+        self.ui.avrdudeParams.setText(param)
+
+    def save_settings(self):
+        fd = 6
 
 
 ORGANIZATION_NAME = 'SUSU'
